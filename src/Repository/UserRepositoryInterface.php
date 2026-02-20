4aed297d2d1eedbcf7cf73a9d91715e5b43b7b5e
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2026-02-20 15:57:04 -0500

    Adding repository interface to retrieve an identity by user id
    Signed-off-by: Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>

--- src/Repository/UserRepositoryInterface.php
+++ src/Repository/UserRepositoryInterface.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Lmc\Api\Auth\Repository;
 
-use Lmc\Api\Auth\Identity\IdentityInterface;
-
 interface UserRepositoryInterface
 {
-    public function getByUserId(int|string $userId): ?IdentityInterface;
+    public function getByUserId(int|string $userId): mixed;
 }
